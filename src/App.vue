1d40ec6e15e2456964db451ea830048f1f139aa6
Dzianis Laurentsyeu <146071428+Dznslav@users.noreply.github.com>
2024-10-20 22:42:21 +0200

    Add files via upload

---
+++ src/App.vue
@@ -0,0 +1,26 @@
+<template>
+  <img alt="Vue logo" src="./assets/logo.png">
+  <TestComponent/>
+  
+</template>
+
+<script>
+import TestComponent from './components/TestComponent.vue';
+
+
+export default {
+  name: 'App',
+  components: {
+    TestComponent
+  }
+}
+</script>
+
+<style>
+#app {
+  font-family:'Courier New', Courier, monospace;
+  color: #2c3e50;
+  margin-top: 60px;
+  text-align: center;
+}
+</style>
---
+++ src/components/TestComponent.vue
@@ -0,0 +1,19 @@
+<template>
+  <div>
+    <h1>UGYSVCGUSBCUGS</h1>
+  </div>
+</template>
+
+<script>
+export default {
+  name: 'TestComponent',
+  props: {
+    msg: String
+  }
+}
+</script>
+
+
+<style scoped>
+
+</style>
