5d78137ad2f09da1519df6d1d8abfa9061f34e02
Dzianis Laurentsyeu <146071428+Dznslav@users.noreply.github.com>
2025-01-15 13:48:29 +0100

    Add files via upload

---
+++ src/App.vue
@@ -0,0 +1,22 @@
+<template>
+
+<LandingPage />
+
+</template>
+
+<script>
+import LandingPage from './components/LandingPage.vue';
+
+export default {
+  name: "App",
+  components: {
+    LandingPage,
+  },
+};
+
+
+</script>
+
+<style>
+
+</style>
---
+++ src/components/LandingPage.vue
@@ -0,0 +1,20 @@
+<template>
+    <div class="landing-page">
+      <h1>Welcome to Moodscape!</h1>
+
+    </div>
+  </template>
+  
+  <script>
+  export default {
+    name: 'LandingPage',
+  };
+  </script>
+  
+  <style scoped>
+  .landing-page {
+    text-align: center;
+    margin-top: 50px;
+  }
+
+  </style>
